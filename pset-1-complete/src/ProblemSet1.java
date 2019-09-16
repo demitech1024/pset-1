a825660751b90140810c7800a84a7dfe471366e5
demitech1024 <demitech1024@gmail.com>
2019-09-16 08:44:02 -0400

    Almost complete with pset1. I have to make TRUE and FALSE for exercise 9 print in lowercase, and I need to make sure spacing is correct.

--- pset-1-complete/src/ProblemSet1.java
+++ pset-1-complete/src/ProblemSet1.java
@@ -22,19 +22,21 @@ import java.text.DecimalFormat;
 public class ProblemSet1 {
     
     public static void main(String[] args) {
-        double inchToCentimeter = 2.54;
         /*
          * Exercise 1.
          * 
          * What is the area (in square millimeters) of an 8.5-by-11-inch sheet of paper?
          */
         
+        final double inchToMillimeter = 2.54 * 10;
+        final double width = 8.5;
+        final double length = 11;
         NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.US);
         DecimalFormat decimalFormat = new DecimalFormat("0.00");
-        double sheetAreaMillimeter = (8.5 * inchToCentimeter * 10) * (11 * inchToCentimeter * 10);
+        double sheetAreaMillimeter = (width * inchToMillimeter) * (length * inchToMillimeter);
         sheetAreaMillimeter = Double.parseDouble(decimalFormat.format(sheetAreaMillimeter));
         String stringAreaMillimeter = numberFormat.format(sheetAreaMillimeter);
-        System.out.printf("\n%S square millimeters.\n", stringAreaMillimeter);
+        System.out.printf("\n%S square millimeters.\n\n", stringAreaMillimeter);
 
         
         /*
@@ -43,9 +45,10 @@ public class ProblemSet1 {
          * What is the perimeter (in centimeters) of an 8.5-by-11-inch sheet of paper?
          */
         
-        double sheetPerimiter = (8.5 * 2 * inchToCentimeter) + (11 * 2 * inchToCentimeter);
-        System.out.printf("\n%.2f centimeters.\n", sheetPerimiter);
-;        
+        double inchToCentimeter = inchToMillimeter / 10;
+        double sheetPerimiter = (width * 2 * inchToCentimeter) + (length * 2 * inchToCentimeter);
+        System.out.printf("\n%.2f centimeters.\n\n", sheetPerimiter);
+;       
         /*
          * Exercise 3.
          * 
@@ -53,8 +56,8 @@ public class ProblemSet1 {
          * by-11-inch sheet of paper?
          */
         
-        double diagonalLength = Math.sqrt((Math.pow(8.5, 2) + Math.pow(11, 2)));
-        System.out.printf("\n%.2f inches.\n", diagonalLength);
+        double diagonalLength = Math.sqrt((Math.pow(width, 2) + Math.pow(length, 2)));
+        System.out.printf("\n%.2f inches.\n\n", diagonalLength);
 
         /*
          * Exercise 4.
@@ -63,9 +66,9 @@ public class ProblemSet1 {
          * what marking period grade will I get?
          */
         
-        double quizPercent = 0.35;
-        double testPercent = 0.50;
-        double homeworkPercent = 0.15;
+        final double quizPercent = 0.35;
+        final double testPercent = 0.50;
+        final double homeworkPercent = 0.15;
         int homework1 = 88;
         int homework2 = 91;
         int homework3 = 0;
@@ -75,9 +78,12 @@ public class ProblemSet1 {
         int test1 = 74;
         int test2 = 87;
         int test3 = 82;
+        double testContrib = (testPercent * (((double) (test1 + test2 + test3)) / 3));
+        double quizContrib = (quizPercent * (((double) (quiz1 + quiz2 + quiz3)) / 3));
+        double homeworkContrib = (homeworkPercent * (((double) (homework1 + homework2 + homework3)) / 3));
 
-        double finalGrade = (testPercent * ((test1 + test2 + test3) / 3)) + (quizPercent * ((quiz1 + quiz2 + quiz3) / 3)) + (homeworkPercent * ((homework1 + homework2 + homework3) / 3));
-        System.out.printf("\n%.2f%%\n", finalGrade);
+        double finalGrade = testContrib + quizContrib + homeworkContrib;
+        System.out.printf("\n%.2f%%.\n\n", finalGrade);
         
         
         /*
@@ -87,9 +93,10 @@ public class ProblemSet1 {
          * will I make this week?
          */
 
+        final double hourlyWage = 12.50;
         double totalHours = 7.5 + 8 + 10.5 + 9.5 + 6 + 11.5;
-        double moneyMade = totalHours * 12.5;
-        System.out.printf("\n$%.2f.\n", moneyMade);
+        double moneyMade = totalHours * hourlyWage;
+        System.out.printf("\n$%.2f.\n\n", moneyMade);
         
         /*
          * Exercise 6.
@@ -97,13 +104,13 @@ public class ProblemSet1 {
          * What is my take-home pay each check?
          */
         
-        double fedIncomeTax = 0.24;
-        double stateIncomeTax = 0.0637;
-        double fourZeroOneKContrTax = 0.07;
-        double salary = 117000.00;
+        final double fedIncomeTax = 0.24;
+        final double stateIncomeTax = 0.0637;
+        final double fourZeroOneKContrTax = 0.07;
+        final double salary = 117000.00;
         double fullPaycheck = salary / 24;
         double payCheck = Double.parseDouble(decimalFormat.format(fullPaycheck * (1 - (fedIncomeTax + stateIncomeTax + fourZeroOneKContrTax))));
-        System.out.printf("\n$%S.\n", (numberFormat.format(payCheck)));
+        System.out.printf("\n$%S.\n\n", (numberFormat.format(payCheck)));
 
         
         /*
@@ -113,12 +120,12 @@ public class ProblemSet1 {
          * people will be on the last bus?
          */
         
-        int numStudents = 273;
-        int numTeachers = 28;
-        double busCapacity = 54;
-        int bussesFilled = (int) Math.ceil((numStudents + numTeachers) / busCapacity);
-        int leftOver = (int) ((numStudents + numTeachers) % busCapacity);
-        System.out.printf("\n%d buses are needed, with %d passengers on the last bus.\n", bussesFilled, leftOver);
+        final int numStudents = 273;
+        final int numTeachers = 28;
+        final int busCapacity = 54;
+        int bussesFilled = (int) Math.ceil(((double) (numStudents + numTeachers)) / ((double) busCapacity));
+        int leftOver = (int) (((double)(numStudents + numTeachers)) % ((double) busCapacity));
+        System.out.printf("\n%d buses are needed, with %d passengers on the last bus.\n\n", bussesFilled, leftOver);
 
         /*
          * Exercise 8.
@@ -126,22 +133,43 @@ public class ProblemSet1 {
          * What is the surface area of a standard Cornhole board?
          */
         
-        
+        final double length2 = 48;
+        final double width2 = 24;
+        final double diameter = 6;
+        double rectangleArea = length2 * width2;
+        double circleArea = Math.PI * Math.pow(diameter / 2, 2);
+        double cornholeArea = rectangleArea - circleArea;
+        cornholeArea = Double.parseDouble(decimalFormat.format(cornholeArea));
+        String cornholeString = numberFormat.format(cornholeArea);
+        System.out.printf("\n%S square inches.\n\n", cornholeString);
         
         /*
-         * Exercise 9.
+         * Exercise 9. 
          * 
          * Are the years 2020, 2100, and 2400 leap years?
          */
         
-        
+        int twenty = 2020;
+        int oneHundred = 2100;
+        int fourHundred = 2400;
+        boolean twentyLeap = true;
+        boolean oneHundredLeap = false;
+        boolean fourHundredLeap = true;
+        String twentyLeapString = String.valueOf(twentyLeap).toLowerCase();
+        String hundredLeapString = String.valueOf(oneHundredLeap).toLowerCase();
+        String fourHundredLeapString = String.valueOf(fourHundredLeap).toLowerCase();
+        System.out.printf("\n%d is a leap year...%S.\n%d is a leap year...%S.\n%d is a leap year...%S.\n\n", twenty, twentyLeapString, oneHundred, hundredLeapString, fourHundred, fourHundredLeapString);
+    
         
         /*
          * Exercise 10.
          * 
          * What is the wind chill?
          */
-        
+        double t = 38.00;
+        double v = 14.00;
+        double windChill = 35.74 + (0.6215 * t) + (((0.4275 * t) - 35.75) * Math.pow(v, 0.16));
+        System.out.printf("\n%.1f degrees.\n\n", windChill);
         
         
     }
